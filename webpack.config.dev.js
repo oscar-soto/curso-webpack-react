84dd2560df7a465134979492475deeddf90e8d1b
oscar-sto <oscarantonio.soto1998@gmail.com>
2022-04-18 13:16:03 -0400

    Feat: add optimization

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,24 +1,17 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
-        publicPath: "/",
-        clean: true,
     },
     resolve: {
         extensions: [".js", ".jsx"],
-        alias: {
-            '@components': path.resolve(__dirname, 'src/components/'),
-            '@styles': path.resolve(__dirname, 'src/styles/'),
-        }
     },
-    mode: 'production',
+    mode: "deveploment",
     module: {
         rules: [
             {
@@ -37,7 +30,7 @@ module.exports = {
             {
                 test: /\.(css|scss)$/i,
                 use: [
-                    MiniCssExtractPlugin.loader,
+                    'style-loader',
                     'css-loader',
                     'sass-loader'
                 ]
@@ -53,10 +46,11 @@ module.exports = {
             filename: '[name].css'
         })
     ],
-    optimization : {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin()
-        ]
-    }
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        compress: true,
+        port: 3006,
+    },
 };
